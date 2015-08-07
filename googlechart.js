546b86f91cb6b68e6625b9d010c84c712b0183bc
Core Ramiro <coreramiro@gmail.com>
2015-08-07 15:11:45 +0100

    Adding layout

--- googlechart.js
+++ googlechart.js
@@ -2,6 +2,11 @@ var express = require( 'express' );
 
 var app = express();
 
+    // set up handlebars view engine
+var handlebars = require( 'express3-handlebars' ) .create( { defaultLayout:'main' } );
+   app.engine( 'handlebars', handlebars.engine );
+   app.set( 'view engine', 'handlebars' );
+
 app.set( 'port', process.env.PORT || 8080 );
 
 app.use(express.static(__dirname + '/public'));
