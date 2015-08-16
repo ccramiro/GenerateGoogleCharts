8ff87121307c6b282d47622ea4e01014d59186fd
Core Ramiro <coreramiro@gmail.com>
2015-08-16 19:22:49 +0100

    Adding simple AngularJS controller

---
+++ public/core.js
@@ -0,0 +1,13 @@
+( function() {
+   var app = angular.module( 'appGC', [ ] );
+
+   app.controller( 'LineChartController', function(){
+      this.point = item;
+   });
+
+   var item = {
+      x: 200,
+      y: 100,
+   }
+
+})();
